dec035abb0a9a93f6650f113d0f7879c8162e141
Milan Bontridder <119121532+mbontrid@users.noreply.github.com>
2026-04-21 11:47:40 +0200

    code: repair broken includes

--- kernels/main.cuh
+++ kernels/main.cuh
@@ -1,6 +1,4 @@
 #pragma once
-#include <cuda_runtime.h>
-#include <cuda.h>
 
 // This is the public interface of our cuda function, called directly in main.cpp
 void wrap_test_vectorAdd();
--- src/constants.hpp
+++ src/constants.hpp
@@ -1,6 +1,7 @@
-#include <vector>
-#include <string>
+#include "cam_params.hpp"
 #include <opencv2/core/mat.hpp>
+#include <string>
+#include <vector>
 
 const float ZNear = 0.3f;
 const float ZFar = 1.1f;
@@ -8,15 +9,16 @@ const int ZPlanes = 256;
 
 typedef unsigned char u_char;
 
-struct cam
-{
-    std::string name;
-    int width;
-    int height;
-    int size;
-    std::vector<cv::Mat> YUV;
-    params<double> p;
-    cam() : name(""), width(-1), height(-1), size(-1), YUV(), p(){};
-    cam(std::string _name, int _width, int _height, int _size, std::vector<cv::Mat> &_YUV, params<double> &_p)
-        : name(_name), width(_width), height(_height), size(_size), YUV(_YUV), p(_p){};
+struct cam {
+  std::string name;
+  int width;
+  int height;
+  int size;
+  std::vector<cv::Mat> YUV;
+  params<double> p;
+  cam() : name(""), width(-1), height(-1), size(-1), YUV(), p() {};
+  cam(std::string _name, int _width, int _height, int _size,
+      std::vector<cv::Mat> &_YUV, params<double> &_p)
+      : name(_name), width(_width), height(_height), size(_size), YUV(_YUV),
+        p(_p) {};
 };
